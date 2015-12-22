c16d31cfe36613c388d14b350efb8be3216dc65c
eb <eb@ebPC>
2015-12-22 18:13:41 +0100

    selection

--- public_html/js/ebtable.js
+++ public_html/js/ebtable.js
@@ -49,7 +49,7 @@ $.fn.ebtable = function (opts, data) {
    var pageCur = 0;
    var defopts = {
       bodyheight: Math.max(200, $(window).height() - 10)
-      , rowsPerPageSelect: [10, 25, 50, 100]
+      , rowsPerPageSelectValues: [10, 25, 50, 100]
       , rowsPerPage: 10
       , colorder: _.range(opts.columns.length) // [0,1,2,... ]
       , sortmaster: null //[{col:1,order:asc,format:fct1},{col:2}]
@@ -67,16 +67,17 @@ $.fn.ebtable = function (opts, data) {
       var res = '';
       var order = myopts.colorder;
       if (myopts.selection) {
-         res += '<th><input type="checkbox" id="select"></th>';
+         res += '<th><input type="checkbox" id="selectHead"></th>';
       }
       for (var c = 0; c < myopts.columns.length; c++) {
          var col = myopts.columns[order[c]];
          if (!col.invisible) {
             res += '   <th id="' + col.name + '">'
-                    + '   <input type="text" id="' + col.name + '" />'
-                    + '   <div class="sort_wrapper">' + col.name
-                    + '      <span class="ui-icon ui-icon-triangle-2-n-s">'
+                    + '   <div class="sort_wrapper">'
+                    + '      <span class="ui-icon ui-icon-triangle-2-n-s"/>'
+                    + col.name
                     + '   </div>'
+                    + '   <input type="text" id="' + col.name + '" />'
                     + '</th>';
          }
       }
@@ -90,7 +91,7 @@ $.fn.ebtable = function (opts, data) {
       for (var r = startRow; r < Math.min(startRow + myopts.rowsPerPage, tblData.length); r++) {
          res += '<tr>';
          if (myopts.selection) {
-            res += '<td><input type="checkbox" id="select"></td>';
+            res += '<td><input type="checkbox" id="select' + r + '"></td>';
          }
          for (var c = 0; c < myopts.columns.length; c++) {
             if (!myopts.columns[order[c]].invisible) {
@@ -115,7 +116,7 @@ $.fn.ebtable = function (opts, data) {
 
    function selectLenCtrl() {
       var options = '';
-      $.each(myopts.rowsPerPageSelect, function (idx, o) {
+      $.each(myopts.rowsPerPageSelectValues, function (idx, o) {
          var selected = o === myopts.rowsPerPage ? 'selected' : '';
          options += '<option value="' + o + '" ' + selected + '>' + o + '</option>\n';
       });
@@ -124,17 +125,18 @@ $.fn.ebtable = function (opts, data) {
 
    function pageBrowseCtrl() {
       return '\n\
-              <button class="firstBtn"><span class="ui-icon ui-icon-seek-first"></span></button>\n\
-              <button class="backBtn"><span  class="ui-icon ui-icon-seek-prev"></span></button>\n\
-              <button class="nextBtn"><span  class="ui-icon ui-icon-seek-next"></span></button>\n\
-              <button class="lastBtn"><span  class="ui-icon ui-icon-seek-end"></span></button>';
+              <button class="firstBtn"><span class="ui-icon ui-icon-seek-first"/></button>\n\
+              <button class="backBtn"><span  class="ui-icon ui-icon-seek-prev"/></button>\n\
+              <button class="nextBtn"><span  class="ui-icon ui-icon-seek-next"/></button>\n\
+              <button class="lastBtn"><span  class="ui-icon ui-icon-seek-end"/></button>';
    }
 
    function infoCtrl() {
       var startRow = myopts.rowsPerPage * pageCur + 1;
       var endRow = Math.min(startRow + myopts.rowsPerPage - 1, tblData.length);
-      var templ = _.template("<%=start%> bis <%=end%>  von <%=count%>");
-      var label = templ({start: startRow, end: endRow, count: tblData.length});
+      var filtered = origData.length === tblData.length ? "" : " (gefiltert von " + origData.length + " Einträgen)";
+      var templ = _.template("<%=start%> bis <%=end%>  von <%=count%> Einträgen <%= filtered %>");
+      var label = templ({start: startRow, end: endRow, count: tblData.length, filtered: filtered});
       return '<button id="info">' + label + '</button>';
    }
 
@@ -170,32 +172,41 @@ $.fn.ebtable = function (opts, data) {
       return true;
    }
 // ##############################################################################
-
-   function adjustHeader() {
-      console.log('>>>adjustHeader window-width=', $(window).width());
-      $('#divall').width($(window).width() - 25);
-      $('#head').width($('#divall').width() - 20);
-      $('#data').width($('#divall').width() - 20);
-
+   function adjustColumns() {
       if (myopts.selection) {
          $('#head th:first').width(20);
          $('#data tr:first td:first').width(20);
       }
       for (var i = (myopts.selection ? 2 : 1); i <= opts.columns.length + (myopts.selection ? 1 : 0); i++) {
-         var w1 = $('#head th:nth-child(' + i + ')').width();
-         var w2 = $('#data td:nth-child(' + i + ')').width();
+         var w1 = $('#head th:nth-child(' + i + ')').innerWidth();
+         var w2 = $('#data td:nth-child(' + i + ')').innerWidth();
          var w = Math.max(w1, w2);
-         console.log(i, 'head:', w1, 'data:', w2, 'max:', w);
-         $('#head th:nth-child(' + i + ')').width(w);
-         $('#data tr:first td:nth-child(' + i + ')').width(w);
+         //console.log(i, 'head:', w1, 'data:', w2, 'max:', w);
+         $('#head th:nth-child(' + i + ')').innerWidth(w);
+         $('#data tr:first td:nth-child(' + i + ')').innerWidth(w);
       }
+      for (var i = (myopts.selection ? 2 : 1); i <= opts.columns.length + (myopts.selection ? 1 : 0); i++) {
+         var w1 = $('#head th:nth-child(' + i + ')').innerWidth();
+         var w2 = $('#data td:nth-child(' + i + ')').innerWidth();
+         if (w1 !== w2) {
+            console.log('Aua!', i, 'head:', w1, 'data:', w2);
+            $(document).width($(document).width() + 100);
+            //adjustColumn();
+         }
+      }
+   }
+   function adjustTable() {
+      console.log('>>>adjustTable window-width=', $(window).width());
+      $('#divall').width($(window).width() - 25);
+      $('#head').width($('#divall').width() - 20);
+      $('#data').width($('#divall').width() - 20);
+      adjustColumns();
       $('#ctrlPage1').css('position', 'absolute').css('top', 5);
       $('#ctrlPage1').css('position', 'absolute').css('right', $(document).width() - $('#data').width() - 10);
       $('#ctrlPage2').css('position', 'absolute').css('right', $(document).width() - $('#data').width() - 10);
    }
-   ;
-   // ##############################################################################
 
+   // ##############################################################################
 
    var tableTemplate = _.template(
            "<div>\n\
@@ -225,7 +236,7 @@ $.fn.ebtable = function (opts, data) {
       , info: infoCtrl()
       , bodyheight: myopts.bodyheight
    }));
-   adjustHeader();
+   adjustTable();
 
    // #################################################################
    // Actions
@@ -245,84 +256,80 @@ $.fn.ebtable = function (opts, data) {
               }
            });
 
-   $('.firstBtn').button()
-           .click(function () {
-              pageCur = 0;
-              var newrows = tableData(pageCur);
-              $('#data tbody').html(newrows);
-              $('#ctrlInfo').html(infoCtrl());
-              $(window).trigger('resize');
-           });
-   $('.backBtn').button()
-           .click(function () {
-              pageCur = Math.max(0, pageCur - 1);
-              var newrows = tableData(pageCur);
-              $('#data tbody').html(newrows);
-              $('#ctrlInfo').html(infoCtrl());
-              $(window).trigger('resize');
-           });
-   $('.nextBtn').button()
-           .click(function () {
-              if (tblData.length === 0)
-                 return;
-              var maxPageCur = Math.floor(tblData.length / myopts.rowsPerPage);
-              pageCur = $.fn.ebtableHelpers.clip(pageCur + 1, 0, maxPageCur);
-              var newrows = tableData(pageCur);
-              $('#data tbody').html(newrows);
-              $('#ctrlInfo').html(infoCtrl());
-              $(window).trigger('resize');
-           });
-   $('.lastBtn').button()
-           .click(function () {
-              pageCur = Math.floor(tblData.length / myopts.rowsPerPage);
-              var newrows = tableData(pageCur);
-              $('#data tbody').html(newrows);
-              $('#ctrlInfo').html(infoCtrl());
-              $(window).trigger('resize');
-           });
+   $('.firstBtn').button().on('click', function () {
+      pageCur = 0;
+      var newrows = tableData(pageCur);
+      $('#data tbody').html(newrows);
+      $('#ctrlInfo').html(infoCtrl());
+      $(window).trigger('resize');
+   });
+   $('.backBtn').button().on('click', function () {
+      pageCur = Math.max(0, pageCur - 1);
+      var newrows = tableData(pageCur);
+      $('#data tbody').html(newrows);
+      $('#ctrlInfo').html(infoCtrl());
+      $(window).trigger('resize');
+   });
+   $('.nextBtn').button().on('click', function () {
+      if (tblData.length === 0)
+         return;
+      var maxPageCur = Math.floor(tblData.length / myopts.rowsPerPage);
+      pageCur = $.fn.ebtableHelpers.clip(pageCur + 1, 0, maxPageCur);
+      var newrows = tableData(pageCur);
+      $('#data tbody').html(newrows);
+      $('#ctrlInfo').html(infoCtrl());
+      $(window).trigger('resize');
+   });
+   $('.lastBtn').button().on('click', function () {
+      pageCur = Math.floor(tblData.length / myopts.rowsPerPage);
+      var newrows = tableData(pageCur);
+      $('#data tbody').html(newrows);
+      $('#ctrlInfo').html(infoCtrl());
+      $(window).trigger('resize');
+   });
 
-   $('#head th')
-           .on('click', function (event, selector, data) {
-              console.log('click', event.currentTarget.id);
-              if (!event.currentTarget.id)
-                 return
-              var idx = indexOfCol(event.currentTarget.id);
-              var col = myopts.columns[idx];
-              var coldefs = (myopts.sortmaster && idx !== myopts.sortmaster[0].col) ? $.extend([], myopts.sortmaster) : [];
-              coldefs.push({col: idx, format: col.format, order: opts.columns[idx].order});
-              tblData = $.fn.ebtableSort.sort(tblData, coldefs);
-              $.each(coldefs, function (idx, o) {
-                 myopts.columns[o.col].order = opts.columns[o.col].order === 'desc' ? 'asc' : 'desc';
-              });
-              pageCur = 0;
-              var newrows = tableData(pageCur);
-              $('#data tbody').html(newrows);
-              $('#ctrlInfo').html(infoCtrl());
-              var cls1 = col.order === 'asc' ? 'ui-icon-triangle-1-s' : 'ui-icon-triangle-1-n';
-              $('#head div span').removeClass('ui-icon-triangle-1-n').removeClass('ui-icon-triangle-1-s').addClass('ui-icon-triangle-2-n-s');
-              $('#head #' + event.currentTarget.id + ' div span').removeClass('ui-icon-triangle-2-n-s').addClass(cls1);
-              $(window).trigger('resize');
-           });
-   $('#info')
-           .button();
-   $('#head input')
-           .on('keyup', function (event) {
-              console.log('filter', event);
-              filterData();
-              pageCur = 0;
-              $('#data tbody').html(tableData(pageCur));
-              $('#ctrlInfo').html(infoCtrl());
-              $(window).trigger('resize');
-           })
-           .on('click', function (event) {
-              event.target.focus();
-              return false; // ignore - sorting
-           });
+   $('#head th').on('click', function (event, selector, data) {
+      console.log('click', event.currentTarget.id);
+      if (!event.currentTarget.id)
+         return;
+      var idx = indexOfCol(event.currentTarget.id);
+      var col = myopts.columns[idx];
+      var coldefs = (myopts.sortmaster && idx !== myopts.sortmaster[0].col) ? $.extend([], myopts.sortmaster) : [];
+      coldefs.push({col: idx, format: col.format, order: opts.columns[idx].order});
+      tblData = $.fn.ebtableSort.sort(tblData, coldefs);
+      $.each(coldefs, function (idx, o) {
+         myopts.columns[o.col].order = opts.columns[o.col].order === 'desc' ? 'asc' : 'desc';
+      });
+      pageCur = 0;
+      var newrows = tableData(pageCur);
+      $('#data tbody').html(newrows);
+      $('#ctrlInfo').html(infoCtrl());
+      var cls1 = col.order === 'asc' ? 'ui-icon-triangle-1-s' : 'ui-icon-triangle-1-n';
+      $('#head div span').removeClass('ui-icon-triangle-1-n').removeClass('ui-icon-triangle-1-s').addClass('ui-icon-triangle-2-n-s');
+      $('#head #' + event.currentTarget.id + ' div span').removeClass('ui-icon-triangle-2-n-s').addClass(cls1);
+      $(window).trigger('resize');
+   });
+   $('#head input[type=text]').on('keyup', function (event) {
+      console.log('filter', event);
+      filterData();
+      pageCur = 0;
+      $('#data tbody').html(tableData(pageCur));
+      $('#ctrlInfo').html(infoCtrl());
+      $(window).trigger('resize');
+   }).on('click', function (event) {
+      event.target.focus();
+      return false; // ignore - sorting
+   });
+   $('#data input[type=checkbox]').on('change', function (event) {
+      console.log('change!', event)
+   });
+
+   $('#info').button();
 
    $(window)
            .on('resize', function () {
               console.log('resize!!!');
-              adjustHeader();
+              adjustTable();
            });
    return this;
 };

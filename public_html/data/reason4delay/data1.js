6065e0d500fdfa14aa852502ca87e31c6d3bf438
A403163 <A403163@MC0XUXJC.ww930.my-it-solutions.net>
2017-08-30 12:33:04 +0200

    au

--- public_html/data/reason4delay/data1.js
+++ public_html/data/reason4delay/data1.js
@@ -1,20 +1,27 @@
 var dataReasons4Delay = {
+  au: false,
   r4d : [
     {
-        id: 1, 
-        text: 'huhu',
+        id: 11, 
+        text: 'huhu1',
         dateCapture: '01.01.2017',
         dateCreate: '01.01.2017',
         dateShipping: '28.02.2017',
+        dateMeeting:  null,//'28.02.2017', 
+        timeMeeting:  null,//'10:45',
+        idhelpdeskMeeting:null,
         idIsmed3: 357,
     },
     {
-        id: 2, 
-        text: 'huhu',
-        dateCapture: '01.01.2017',
-        dateCreate: '01.01.2017',
-        dateShipping: '28.02.2017',
-        idIsmed3: 357,
+        id: 21, 
+        text: 'huhu2',
+        dateCapture: '01.01.2018',
+        dateCreate: '01.01.2018',
+        dateShipping: '28.02.2018',
+        dateMeeting:  '28.02.2018', 
+        timeMeeting:  '10:45', 
+        idhelpdeskMeeting:  25, 
+        idIsmed3: 234,
     },
   ]
 }
